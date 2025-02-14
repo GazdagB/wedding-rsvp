c435f74a2b29a11e0a178c35cd27698adc9eecc0
GazdagB <gazdagbal@gmail.com>
2025-02-14 22:08:16 +0100

    💄 Adjusting z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="fixed items-center flex justify-between top-0 left-1/2 transform -translate-x-1/2 bg-white py-10 w-[1200px]">
+    <nav className="fixed items-center flex justify-between top-0 left-1/2 transform -translate-x-1/2 bg-white py-10 pb-15 w-[1200px] z-20">
         {/* Menu */}
         <div className="gap-3 flex max">
             <Link to="/"  className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Rólunk</Link>
@@ -16,7 +16,7 @@ const Navbar = () => {
         <div className="logo text-5xl mr-30 bg-clip-text text-transparent bg-linear-to-r from-[#BF953F] via-[#dddab2]  to-[#AA771C]">
             Aletta & Balázs
         </div>
-        <p className="logo absolute bottom-0 left-[50%] transform -translate-x-[30%] text-[20px]">
+        <p className="logo absolute bottom-5 left-[50%] transform -translate-x-[30%] text-[20px]">
             Szeptember 7. 2025
         </p>
 
