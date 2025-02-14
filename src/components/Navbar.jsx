04e79ee8899712d8a42c984f859ef1401bbeae48
GazdagB <gazdagbal@gmail.com>
2025-02-14 19:35:42 +0100

    💄 Style and place the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav>
-        <NavLink to="/"  className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Home</NavLink>
-        <NavLink to="/rsvp" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>RSVP</NavLink>
-        <NavLink to="/admin" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Admin</NavLink>
+    <nav className="flex items-center justify-between py-7 px-10">
+        {/* Menu */}
+        <div className="gap-3 flex max">
+            <Link to="/"  className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Rólunk</Link>
+            <Link to="/#helyszin" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Helyszín</Link>
+            <Link to="/#datum" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Dátum</Link>
+            <Link to="/#program" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Program</Link>
+            <Link to="/#foto" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Fotó album</Link>
 
-        <div className="logo text-2xl">
+        </div>
+
+        <div className="logo text-5xl mr-30">
             Aletta & Balázs
+        </div>
+
+        <div className="flex gap-5 items-center">
+            <Link>GYIK</Link>
+            <Link className="border border-gray-500 p-2 rounded-md" to="/rsvp">Ott leszek!</Link>
         </div>
     </nav>
   )
