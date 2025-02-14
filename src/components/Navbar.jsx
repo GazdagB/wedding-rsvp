438a3ce9918282d746be8333e9ff21d236b626f0
GazdagB <gazdagbal@gmail.com>
2025-02-14 21:31:49 +0100

    💄 Style the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-7 px-10">
+    <nav className="fixed items-center flex justify-between top-0 left-1/2 transform -translate-x-1/2 bg-white py-10 w-[1200px]">
         {/* Menu */}
         <div className="gap-3 flex max">
             <Link to="/"  className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Rólunk</Link>
@@ -13,13 +13,16 @@ const Navbar = () => {
 
         </div>
 
-        <div className="logo text-5xl mr-30">
+        <div className="logo text-5xl mr-30 bg-clip-text text-transparent bg-linear-to-r from-[#BF953F] via-[#dddab2]  to-[#AA771C]">
             Aletta & Balázs
         </div>
+        <p className="logo absolute bottom-0 left-[50%] transform -translate-x-[30%] text-[20px]">
+            Szeptember 7. 2025
+        </p>
 
         <div className="flex gap-5 items-center">
             <Link>GYIK</Link>
-            <Link className="border border-gray-500 p-2 rounded-md" to="/rsvp">Ott leszek!</Link>
+            <Link className="border border-gray-500 p-2 font-semibold rounded-md hover:bg-[#bea794] hover:border-transparent hover:text-white transition duration-300" to="/rsvp">Ott leszek!</Link>
         </div>
     </nav>
   )
