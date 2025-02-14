4b8b503841f6ec9f9aef34d2f50ba6ba4d038da9
GazdagB <gazdagbal@gmail.com>
2025-02-14 17:57:52 +0100

    🎨 Structuring pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+//Dependencie imports 
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
+
+// Page imports 
+import Home from './pages/Home'
+import RSVP from './pages/RSVP'
+import Admin from './pages/Admin'
+
+//Styling import
 import './App.css'
 
 function App() {
 
 
   return (
-    <div className='App'>
-        <h1 className='text-slate-500 font-bold'>Hello,World!</h1>
-    </div>
+    <Router>
+      <nav>
+        <NavLink to="/"  className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Home</NavLink>
+        <NavLink to="/rsvp" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>RSVP</NavLink>
+        <NavLink to="/admin" className={({isActive})=> `text-blue-500 hover:underline ${isActive ? "font-bold text-blue-700" : ""}`}>Admin</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rsvp" element={<RSVP />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   )
 }
 
