2231d0bfb24d5a45d2109ab17c0f007e9fc0e89d
GazdagB <gazdagbal@gmail.com>
2025-02-14 19:08:45 +0100

    🔧 Add tailwind config file for intelisense

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}', // Include all JSX/TSX files
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          playfair: ['Playfair Display', 'serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
