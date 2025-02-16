c4cc6d71f9c08052604e6a5eb3ab87d559ecfc13
GazdagB <gazdagbal@gmail.com>
2025-02-16 20:49:15 +0100

    ✨ Date section

---
+++ src/components/Date.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Date = () => {
+  return (
+    <div className='relative h-[860px] bg-fixed  bg-center bg-cover mb-100 flex items-center justify-center  flex-col'
+    style={{backgroundImage: "url('images/date-bg.jpg')"}}
+    >
+
+        <div className='bg-black inset-0 absolute opacity-50 backdrop-blur-lg z-0'></div>
+        <h2 className='dancing text-6xl text-white z-10 mb-10'>Az időpont</h2>
+        <img className='z-10 mb-[68px]' src="/images/calendar.png" alt="" />
+        <h3 className='text-white text-[56px] dancing z-10'>2025 Szeptember 07. Vasárnap </h3>
+
+    </div>
+  )
+}
+
+export default Date
