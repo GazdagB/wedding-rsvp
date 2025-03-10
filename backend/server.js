16f013b7b460cdea71f97bccd1c0f8a65ced66c4
GazdagB <gazdagbal@gmail.com>
2025-03-10 22:37:19 +0100

    ✨ Added validator

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
+const validator = require('validator'); 
 
 const app = express(); 
 const PORT = process.env.PROT || 5000; 
@@ -10,6 +11,8 @@ const invitedGuests = [
     'jane smith',
     'mark johnson'
 ]
+
+let rsvpList = []; 
 
 //Middleware 
 app.use(cors()); 
@@ -29,6 +32,16 @@ app.post('/rsvp',(req,res)=>{
         return res.status(400).json({message: 'Your name is not on the guest list'});
     }
 
+    if(!validator.isEmail(email)){
+        return res.status(400).json({message: 'Please provide a valid email adress.'})
+    }
+
+    const existingRSVP = rsvpList.find(rsvp => rsvp.email === email);
+
+    if (existingRSVP) {
+        return res.status(400).json({ message: 'You have already submitted your RSVP.' });
+    }
+
     const rsvpData = {
         fullName,
         accept,
@@ -39,13 +52,14 @@ app.post('/rsvp',(req,res)=>{
         message
     };
 
+    rsvpList.push(rsvpData)
+
     console.log('RSVP recieved:', rsvpData);
 
     res.status(200).json({
         message: 'Thank you for your RSVP!',
         data: rsvpData
     })
-    
 })
 
 app.listen(PORT,()=>{
