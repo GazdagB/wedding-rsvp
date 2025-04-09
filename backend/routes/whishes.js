a17f278701caae9bfafc639b8ecda2b0dae5f838
GazdagB <gazdagbal@gmail.com>
2025-04-09 21:37:47 +0200

    ✨ Restricts number of entries for whishes

--- backend/routes/whishes.js
+++ backend/routes/whishes.js
@@ -1,11 +1,38 @@
 const express = require('express'); 
 const Whish = require('../models/Whishes'); 
+const path = require("path")
+const fs = require("fs")
 
 const router = express.Router();
+
+const invitedGuestsPath = path.join(__dirname, '../data/invitedGuests.json');
+const invitedGuests = JSON.parse(fs.readFileSync(invitedGuestsPath, 'utf8'))
 
 router.post("/",async (req,res)=>{
 
     const {message, author, iconType,password,familyName} = req.body; 
+
+    if(!password){
+        return res.status(400).json({message: 'A jelszó megadása kötelező!'}); 
+    }
+
+    const guest = invitedGuests.find(g => g.familyName === familyName);
+
+    if(!guest){
+       return res.status(400).json({message: 'A neved nem szerepl a listán vagy elírtad.'})
+    }
+
+    if(guest.password !== password){
+        return res.status(400).json({message: 'Hibás jelszó. Kérlek ellenőrizd a meghívódon szereplő jelszót.'}); 
+    }
+
+    const submittedCount = await Whish.find({familyName: familyName})
+    console.log(submittedCount);
+    
+
+    if(submittedCount.length > 3){
+        return res.status(400).json({message: "Elfogytak a beküldhető üzeneteid 3/3 üzenet beküldve."})
+    }
 
     const newWish = new Whish({
         message,
