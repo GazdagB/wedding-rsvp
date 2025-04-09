467e98d1750a6e9549e71bc59ed3ea0709e4ed3f
GazdagB <gazdagbal@gmail.com>
2025-04-09 20:58:51 +0200

    🗃️  Updates Scheemafor Whishes

--- backend/models/Whishes.js
+++ backend/models/Whishes.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const whishesSchema = new mongoose.Schema({
     message: {type: String, required: true},
     author: {type: String, required: true},
-    iconType: {type: String, required:true}
+    iconType: {type: String, required:true},
+    password: {type: String, required: true},
+    familyName: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Whishes', whishesSchema)
