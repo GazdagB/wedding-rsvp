d98f5db0193970e813b934947bdab0b46b1af99d
GazdagB <gazdagbal@gmail.com>
2025-04-20 11:48:46 +0200

    📱 Adds responsivity for sidebar

--- frontend/src/components/Sidebar/AdminLayout.jsx
+++ frontend/src/components/Sidebar/AdminLayout.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
+import {useMediaQuery } from 'react-responsive'; 
 
 const AdminLayout = ({ children }) => {
 
-  const [isOpen,setIsOpen] = useState(false);
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+ 
 
   return (
     <div className="flex min-h-screen">
       {/* Invisible spacer to push content right */}
-      <div className={` ${isOpen ? "w-[280px]" : ""}`} />
+      <div className={` ${!isTabletOrMobile ? "w-[280px]" : ""}`} />
       
       {/* Sidebar is fixed, not part of flow */}
-      <Sidebar isOpen={isOpen} />
+      <Sidebar isOpen={!isTabletOrMobile} />
 
       {/* Main content area */}
       <div className="flex-1 p-10">
