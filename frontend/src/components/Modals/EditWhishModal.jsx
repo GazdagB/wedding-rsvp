e1ccde1bdc038fe38cd8d60451940c2dfd9b8bb3
GazdagB <gazdagbal@gmail.com>
2025-04-22 19:13:37 +0200

    ✨  Prepare whishes to edit on frontend

--- frontend/src/components/Modals/EditWhishModal.jsx
+++ frontend/src/components/Modals/EditWhishModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {motion} from "motion/react"
 import { FaHeart } from "react-icons/fa";
 import { GiClover } from "react-icons/gi";
@@ -8,22 +8,47 @@ import { IoAirplane } from "react-icons/io5";
 import { FaHouse } from "react-icons/fa6";
 import { AnimatePresence } from "motion/react";
 import { BiSolidDownArrow } from "react-icons/bi";
-import { useForm } from "react-hook-form"
+import {axios} from "axios"
 
 
-const EditWhishModal = ({setEditModalOpen}) => {
+const EditWhishModal = ({setEditModalOpen, whishData}) => {
 
-  const [selectedIcon, setSelectedIcon] = useState("heart");
   const [isIconsOpen, setIsIconsOpen] = useState(false);
-  const [author,setAuthor] = useState(""); 
-  const [whish,setWhish] = useState("");
+  const [selectedIcon,setSelectedIcon] = useState("heart"); 
+  const [isDeleteOpen, setDeleteOpen] = useState(false); 
+  const [whish, setWhish] = useState({
+    author: "",
+    message: "",
+  });
+
+  const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+
+    async function handleSubmit(e){
+      e.preventDefault(); 
+
+    }
 
     function closeModal(){
         setEditModalOpen(false)
     }
 
-    
+    async function handleDelete(){
+      
+    }
 
+    function closeDelete(){
+      setDeleteOpen(false);
+    }
+
+    function handleDeleteOpen(e){
+      e.preventDefault(); 
+
+      setDeleteOpen(prev => !prev);
+    }
+
+    useEffect(()=>{
+        setWhish(whishData); 
+    }, [whishData])
    
 
   return (
@@ -37,9 +62,9 @@ const EditWhishModal = ({setEditModalOpen}) => {
     >
         <div onClick={closeModal} className="absolute inset-0 bg-black/50"></div>
 
-        <div className="bg-white w-[500px] rounded-lg shadow-lg z-50 overflow-auto p-6">
+        <div className="bg-white w-[500px] rounded-lg shadow-lg z-50 overflow-auto p-6 scrollbar-hidden">
             <h2 className="font-bold text-2xl">Kívánságok szerketsztése</h2> 
-            <form action="" >
+            <form onSubmit={(e)=>{handleSubmit(e)}} action="" >
             <div className="flex flex-col gap-3 items-center justify-center mt-5">
           <div className="bg-gray-200 w-20 h-20 rounded-full flex items-center justify-center text-wedding-brown text-2xl">
             {/* TODO:  */}
@@ -150,19 +175,59 @@ const EditWhishModal = ({setEditModalOpen}) => {
           </AnimatePresence>
         </div>
 
-        <label htmlFor="">Szerző</label>
-          <input placeholder="Add meg a neved" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-wedding-brown" type="text"
-          value={author}
-          onChange={(e)=>{setAuthor(e.target.value)}}
-          />
-
-          <label htmlFor="">Kívánság</label>
-          <textarea className="block resize-none  w-full h-30 rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-wedding-brown"  type="text" placeholder="Írd le a kívánsáogd..." 
-          value={whish}
-          onChange={(e)=>{setWhish(e.target.value)}}
-          />
+        <div className='flex flex-col gap-4'>
+          <div>
+            <label className='font-bold' htmlFor="">Szerző</label>
+            <input placeholder="Add meg a neved" className="block mt-3 w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-wedding-brown" type="text"
+              value={whish.author}
+              onChange={(e) => {
+                setWhish((prev) => ({
+                  ...prev,
+                  author: e.target.value,
+                }));
+              }}
+              />
+          </div>
+          <div>
+            <label className='font-bold' htmlFor="">Kívánság</label>
+            <textarea className="block resize-none mt-3 w-full h-30 rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-wedding-brown"  type="text" placeholder="Írd le a kívánsáogd..."
+              value={whish.message}
+              onChange={(e) => {
+                setWhish((prev) => ({
+                  ...prev,
+                  message: e.target.value,
+                }));
+              }}
+              />
+          </div>
+        </div>
+          
+          <div className='flex items-center justify-center mt-6'>
+            <button type='submit' className="py-2 px-3 cursor-pointer rounded-md bg-wedding-brown text-white font-bold">Beküldés</button>
+            <button onClick={(e)=>{handleDeleteOpen(e)}} className='text-gray-500 ml-4 cursor-pointer'>Kivánság törlése</button>
+          </div>
             </form>
             
+            <AnimatePresence>
+            {isDeleteOpen && 
+            <motion.div
+            initial={{ y: -20, opacity: 0, height: 0 }}
+            animate={{ y: 0, opacity: 1, height: "auto" }}
+            exit={{ y: -20, opacity: 0, height: 0 }}
+              className='mt-5 flex items-center justify-center'
+            >
+                <div className='gap-4 flex flex-col justify-center items-center'>
+                  <p className='text-center text-gray-500'>A törlés gombra kattintva törölni fogod a kiválasztott kivánságot biztos vagy benne?</p>
+                  <div className='flex items-center jusitfy-center gap-3'>
+                    <button onClick={handleDelete} className="cursor-pointer inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Törlés</button>
+                    <button onClick={closeDelete} className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset cursor-pointer hover:bg-gray-50 sm:mt-0 sm:w-auto">Mégsem</button>
+                  </div>
+                </div>
+            </motion.div>
+            }
+            </AnimatePresence>
+           
+
         </div>
     </motion.div>
   )
--- frontend/src/pages/WhishesPage.jsx
+++ frontend/src/pages/WhishesPage.jsx
@@ -8,6 +8,7 @@ const Whishes = () => {
 
     const [isEditModalOpen, setEditModalOpen] = useState(false); 
     const [whishes, setWhishes] = useState([]); 
+    const [selectedWhish,setSelectedWhish] = useState(0); 
 
     const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
@@ -31,10 +32,14 @@ const Whishes = () => {
     <div className=' pt-20 ps-15  w-full h-full flex items-start justify-center'>
         <div className='grid xl:grid-cols-3 grid-cols-1 lg:grid-cols-2 gap-3 max-w-[1200px]'>
 
-            {isEditModalOpen && <EditWhishModal setEditModalOpen={setEditModalOpen}></EditWhishModal>}
+            {isEditModalOpen && <EditWhishModal whishData={whishes[selectedWhish]} setEditModalOpen={setEditModalOpen}></EditWhishModal>}
 
             {whishes.map((whish,index)=>{
-                return <WhishAdmin setEditModalOpen={setEditModalOpen} author={whish.author} icon={whish.iconType} key={index} text={whish.message} ></WhishAdmin>
+                return (
+                    <div onClick={()=>{setSelectedWhish(index)}} key={index}>
+                        <WhishAdmin setEditModalOpen={setEditModalOpen} author={whish.author} icon={whish.iconType} key={index} text={whish.message} ></WhishAdmin>
+                    </div>
+                )
             })}
         </div>
     </div>
