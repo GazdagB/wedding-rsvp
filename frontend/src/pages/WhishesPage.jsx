0eb6f1fed5a1f0b1e20c263bf13140d87dd47649
GazdagB <gazdagbal@gmail.com>
2025-05-01 17:11:41 +0200

    📱 Whishes fully responsive

--- frontend/src/pages/WhishesPage.jsx
+++ frontend/src/pages/WhishesPage.jsx
@@ -27,7 +27,7 @@ const Whishes = () => {
         }
     }, [isEditModalOpen, API_URL]);
   return (
-    <div className=' pt-20 ps-15  w-full h-full flex items-start justify-center'>
+    <div className='pt-10 md:pt-20 md:ps-15  w-full h-full flex items-start justify-center'>
         <div className='grid xl:grid-cols-3 grid-cols-1 lg:grid-cols-2 gap-3 max-w-[1200px]'>
 
             {isEditModalOpen && <EditWhishModal whishData={whishes[selectedWhish]} setEditModalOpen={setEditModalOpen}></EditWhishModal>}
