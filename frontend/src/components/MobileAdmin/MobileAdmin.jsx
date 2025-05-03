c1070c35632af109bf23b0c9052295e5ab4d016b
GazdagB <gazdagbal@gmail.com>
2025-05-03 17:57:32 +0200

    ✨ Adds mobile menu Animated

--- frontend/src/components/MobileAdmin/MobileAdmin.jsx
+++ frontend/src/components/MobileAdmin/MobileAdmin.jsx
@@ -2,13 +2,40 @@ import { Link, NavLink } from 'react-router-dom';
 import { FaUser,FaStar,FaGlobeAfrica } from "react-icons/fa";
 import { CgLogOut, CgHome } from "react-icons/cg";
 import { HiMenuAlt3 } from "react-icons/hi";
+import {motion, AnimatePresence} from "motion/react";
+import { useState } from "react";
 
 
 const MobileAdmin = () => {
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    const handleNavigate = (path) => {
+        setMenuIsOpen(false); // Close the menu when navigating
+    }
+
   return (
+    <>
+    
+    <AnimatePresence>
+    {
+        menuIsOpen && 
+            <motion.div className='bg-wedding-champagne h-screen w-screen z-50 fixed top-0 left-0'
+            initial={{y: "100%"}}
+            animate={{y: 0}}
+            exit={{y: "100%"}}
+            transition={{duration: 0.3}}
+            >
+
+            </motion.div>
+        
+    }
+   </AnimatePresence>
+
     <div className="fixed flex left-0 bottom-0 w-[100svw] bg-wedding-champagne h-20 z-50">
         <NavLink
          to="/admin" 
+         onClick={() => handleNavigate("/admin")}
          end
          className={({ isActive }) => 
              isActive 
@@ -19,6 +46,7 @@ const MobileAdmin = () => {
         </NavLink>
 
         <NavLink
+        onClick={() => handleNavigate("/admin/guests")}
          to="/admin/guests" 
          className={({ isActive }) => 
              isActive 
@@ -29,6 +57,7 @@ const MobileAdmin = () => {
         </NavLink>
 
         <NavLink
+        onClick={() => handleNavigate("/admin/names")}
          to="/admin/names" 
          className={({ isActive }) => 
              isActive 
@@ -39,6 +68,7 @@ const MobileAdmin = () => {
         </NavLink>
 
         <NavLink
+        onClick={() => handleNavigate("/admin/whishes")}
          to="/admin/whishes" 
          className={({ isActive }) => 
              isActive 
@@ -48,10 +78,11 @@ const MobileAdmin = () => {
            <FaStar></FaStar>
         </NavLink>
 
-        <div className='flex items-center justify-center w-full'>
+        <div onClick={()=>{setMenuIsOpen(prev => !prev)}} className='flex items-center justify-center w-full'>
             <HiMenuAlt3 className="text-3xl text-wedding-brown-darker" />
         </div>
     </div>
+    </>
   )
 }
 
