b72fc409796e8c5e8ee03ac4c14d2cd25af71586
GazdagB <gazdagbal@gmail.com>
2025-05-05 22:49:50 +0200

    📱 Adds responsivity

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-    <div className=''>
-      <h2>I Spy Galéria</h2>
-      <p>Ez a galéria az I Spy játékhoz készült képeket tartalmazza.</p>
+    <div className='flex flex-col items-center justify-center w-full h-full py-10'>
+      <h2 className="dancing text-6xl text-wedding-brown z-10 mb-4">I Spy Galéria</h2>
+      <p className="text-wedding-light-gray mb-10 text-center text-pretty">Ez a galéria az I Spy játékhoz készült képeket tartalmazza.</p>
 
-      <div className='grid grid-cols-5 gap-2 w-max'>
-        <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 w-max '>
+        <div className='flex flex-col gap-2'>   
+            <div className='h-[200px] w-[300px] rounded-md bg-amber-300'></div>
+            <div className='h-[250px] rounded-md w-[300px] bg-teal-300'></div>
+            <div className='h-[200px] rounded-md w-[300px] bg-blue-300'></div>
+            <div className='h-[200px] rounded-md w-[300px] bg-purple-300'></div>
+            <div className='h-[300px] rounded-md w-[300px] bg-rose-300'></div>
+        </div>
+
+        <div className='flex flex-col gap-2' >
             <div className='h-[200px] w-[300px] rounded-md bg-amber-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-teal-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-blue-300'></div>
@@ -15,7 +23,7 @@ const Gallery = () => {
             <div className='h-[300px] rounded-md w-[300px] bg-rose-300'></div>
         </div>
 
-        <div>
+        <div className='flex flex-col gap-2'>
             <div className='h-[200px] w-[300px] rounded-md bg-amber-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-teal-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-blue-300'></div>
@@ -23,7 +31,7 @@ const Gallery = () => {
             <div className='h-[300px] rounded-md w-[300px] bg-rose-300'></div>
         </div>
 
-        <div>
+        <div className='flex flex-col gap-2'>
             <div className='h-[200px] w-[300px] rounded-md bg-amber-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-teal-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-blue-300'></div>
@@ -31,15 +39,7 @@ const Gallery = () => {
             <div className='h-[300px] rounded-md w-[300px] bg-rose-300'></div>
         </div>
 
-        <div>
-            <div className='h-[200px] w-[300px] rounded-md bg-amber-300'></div>
-            <div className='h-[200px] rounded-md w-[300px] bg-teal-300'></div>
-            <div className='h-[200px] rounded-md w-[300px] bg-blue-300'></div>
-            <div className='h-[200px] rounded-md w-[300px] bg-purple-300'></div>
-            <div className='h-[300px] rounded-md w-[300px] bg-rose-300'></div>
-        </div>
-
-        <div>
+        <div className='flex flex-col gap-2'>
             <div className='h-[200px] w-[300px] rounded-md bg-amber-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-teal-300'></div>
             <div className='h-[200px] rounded-md w-[300px] bg-blue-300'></div>
